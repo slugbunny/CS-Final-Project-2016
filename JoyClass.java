8bc929f87abc341e7a536d3267c56e4a68dda13a
mikaelamoore <mikaela.moore@gmail.com>
2016-06-02 13:40:19 -0400

    Update JoyClass.java

--- JoyClass.java
+++ JoyClass.java
@@ -1,1 +1,9 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public static 
